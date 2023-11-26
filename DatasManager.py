45111c2c53d4fe2d3368a6f532af0d279d94d3a4
TGV2107 <122354290+TGV2107@users.noreply.github.com>
2023-11-26 13:21:35 +0100

    Documentation DatasManager

--- DatasManager.py
+++ DatasManager.py
@@ -2,32 +2,67 @@ import csv
 import sqlite3
 
 
-def initDataBase(cursor):
+def initDataBase(cursor : sqlite3.Cursor):
+    """
+    Params :
+    - cursor : sqlite3.Cursor
     
+    Returns :
+    
+    Fonction permettant d'innitialiser les tables non existantes dans la base de donnée"""
+    
+    #Liste des fichiers et de leurs clés
     files = [("Users", "ID_user INT PRIMARY KEY, Name TEXT, Password TEXT, Type INT"),
-             ("Classes","ID_user INT, ID_classe INT, FOREIGN KEY (ID_user) REFERENCES Users(ID_user)")]
+            ("Classes","ID_user INT, ID_classe INT, FOREIGN KEY (ID_user) REFERENCES Users(ID_user)")]
 
     for file in files:
         name, rows = file
 
         cursor.execute(f'''CREATE TABLE IF NOT EXISTS {name}({rows})''')
 
+        #On ouvre le fichier csv correpondant
         with open("Datas/" + name + ".csv") as open_file:
 
             r = csv.reader(open_file)
+            
+            #On ajoute chaque ligne au fichier .db, en utilisant une chaîne de caractère contenant autant de ? que de paramètres devant aller dans la table
             for row in r:
                 args = ', '.join(['?' for _ in row])
                 cursor.execute(f"INSERT INTO {name} VALUES ({args})", row)
 
-def displayDatabase(cursor, table):
+def printTable(cursor : sqlite3.Cursor, table : str):
+    """
+    Params:
+    - cursor : sqlite3.Cursor()
+    - table : str
     
+    Returns :
+    
+    Fontion permettant d'afficher tous ce que contient la table passé en paramètre"""
+    
+    #Récupération des lignes de la table
     cursor.execute(f"SELECT * FROM {table}")
     rows = cursor.fetchall()
 
+    #Affichage des colonnes
     cursor.execute(f"PRAGMA table_info({table})")
     columns = [column[1] for column in cursor.fetchall()]
-
     print(", ".join(columns))
 
+    #Affichage des lignes
     for row in rows:
         print(", ".join(map(str, row)))
+
+def table_exists(cursor : sqlite3.Cursor, table : str) -> bool:
+    """
+    Params :
+    - cursor : sqlite3.Cursor()
+    - table : str
+    
+    Returns :
+    - bool
+    
+    Fonction renvoyant un booléen indiquant si la table existe dans la base de donnée du curseur"""
+
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table'")
+    return table in cursor.fetchone()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sqlite3
 import tkinter
 
 import DatasManager as DM
+import WindowManager as WM
 
 #création de la base de donnée
 connexion = sqlite3.connect('Datas/ESI.db')
@@ -11,8 +12,8 @@ cursor = connexion.cursor()
 #Importation de la base de donnée depuis les csv
 
 DM.initDataBase(cursor)
-DM.displayDatabase(cursor,"Users")
-DM.displayDatabase(cursor,"Classes")
+DM.printTable(cursor,"Users")
+DM.printTable(cursor,"Classes")
 
 window = tkinter.Tk()
 
